5e8cc147f282a8d87860aede29dda4d546e99a4d
dan29iel <95416833+dan29iel@users.noreply.github.com>
2022-10-25 10:59:02 +0200

    Add files via upload

---
+++ UF1/AP2/AP2_Act1_.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package cat.institutmvm.AP2;
+import java.util.Scanner;
+
+/**
+ *
+ * @author alumne_2n
+ */
+public class AP2_Act1_ {
+    private static final String MSG_1 = "Daniel ";
+    private static final String MSG_2 = "Daniel Lora ";
+    private static final String MSG_3 = "Daniel \nLora ";
+    private static final String MSG_4 = "**********\n**********\n**********\n**********\n**********\n**********\n**********\n**********\n**********\n**********\n ";
+    private static final String MSG_5 = "         *\n        * *\n      * * * *\n    * * * * * *\n  * * * * * * * *\n* * * * * * * * * *\n ";
+    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println(MSG_1);
+        System.out.println(MSG_2);
+        System.out.println(MSG_3);
+        System.out.println(MSG_4);
+        System.out.println(MSG_5);
+    }
+}
---
+++ UF1/AP2/AP2_Act2_.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package cat.institutmvm.AP2;
+
+/**
+ *
+ * //Daniel Lora
+ *  Nom:
+    Cognoms:
+    INS Manuel Vázquez Montalbán
+    Data d’edició:
+    Nom del cicle formatiu:
+    Nom del mòdul:
+
+ */
+public class AP2_Act2_ {
+    public static void main(String[] args) {
+       System.out.println("Hello World");
+      
+   }
+}
